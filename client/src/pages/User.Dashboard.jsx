308300f1d60e41b0d735148d3b4d982057ddf073
Webplorer <arfanaali07@gmail.com>
2025-05-02 21:45:29 +0530

    updated dashboard url

--- client/src/pages/User.Dashboard.jsx
+++ client/src/pages/User.Dashboard.jsx
@@ -132,7 +132,7 @@ export default function UserDashboardPage() {
 
   const tasksPerPage = 5;
   const navigate = useNavigate();
-  const API_BASE_URL = "http://localhost:5000/api";
+  const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
   const projectForm = useForm({
     resolver: zodResolver(projectSchema),
