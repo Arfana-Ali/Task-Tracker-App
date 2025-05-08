3af322e5e2c79f2139c68be4a5dfacc954f095fb
Arfana_ali <152651063+Arfana-Ali@users.noreply.github.com>
2025-05-08 17:48:18 +0530

    Update LoginForm.jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -68,15 +68,8 @@ const LoginForm = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      Task Management System
+     
       <Card className="w-full max-w-md shadow-lg">
-{/*         <CardHeader>
-{/*           <CardTitle className="text-2xl text-center text-gray-800"> */}
-{/*           <CardTitle className={`${theme === "dark" ? "text-2xl text-center text-white" : "text-2xl text-center text-gray-800"}`}>
-
-            Log In
-          </CardTitle>
-        </CardHeader> */} 
      <CardHeader>
           <CardTitle
             className={`${theme === "dark" ? "text-center text-white text-2xl font-bold" : "text-center text-primary-gradient text-2xl font-bold"}`}
